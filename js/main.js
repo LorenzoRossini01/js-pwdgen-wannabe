458bb4141de9ab61aaefaa47ed2b6cc5172504ea
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-01-08 15:14:25 +0100

    Svolgimento in javascript

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// # Step 1
+const userName = prompt(
+  "Scrivi il tuo nome per ottenere la tua nuova password per il 2024! "
+);
+console.log(userName);
+
+// # Step 2
+const userSurname = prompt(
+  "Bene" + " " + userName + ", ora inserisci qui il tuo cognome"
+);
+console.log(userSurname);
+
+// # Step 3
+const userColor = prompt(
+  "Ci siamo quasi" +
+    " " +
+    userName +
+    ", ora inserisci qui il tuo colore preferito"
+);
+console.log(userColor);
+
+// # Step 4
+const userPassword = userName + userSurname + userColor + 24;
+console.log(userPassword);
+alert(userPassword);
